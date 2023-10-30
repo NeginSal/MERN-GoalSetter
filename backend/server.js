d43372d9a9e0689fc49b8bfb264b8ddf288dc1bd
NeginSal <neginsaljooghi@gmail.com>
2023-10-30 09:05:03 +0330

    feat:Added goalController

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const port = process.env.PORT || 5000
 const goalRoutes = require('./routes/goalRoutes')
 
 const app = express()
+
 app.use('/api/goals', goalRoutes)
+app.use(express.json())
 
 app.listen(port, () => { console.log(`Server started on port ${port}`) })
