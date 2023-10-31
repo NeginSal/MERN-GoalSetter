20eef96fbab58aaff5b12f1f192b9894d0d998e0
NeginSal <neginsaljooghi@gmail.com>
2023-10-31 15:14:10 +0330

    feat:Added errormiddleware

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,12 @@ const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
 const goalRoutes = require('./routes/goalRoutes')
+const { errorHandler } = require('./middleware/errorMiddleware')
 
 const app = express()
 
 app.use('/api/goals', goalRoutes)
 app.use(express.json())
+app.use(errorHandler)
 
 app.listen(port, () => { console.log(`Server started on port ${port}`) })
