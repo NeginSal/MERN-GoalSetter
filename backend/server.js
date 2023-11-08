a06c813faf155aaf239c3dfbd792a76c8cf3ba76
NeginSal <neginsaljooghi@gmail.com>
2023-11-08 14:51:57 +0330

    feat: Added UserRoutes & UserController

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
 const goalRoutes = require('./routes/goalRoutes')
+const userRoutes = require('./routes/userRoutes')
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 
@@ -10,6 +11,7 @@ connectDB()
 const app = express()
 
 app.use('/api/goals', goalRoutes)
+app.use('./api/users', userRoutes)
 app.use(express.json())
 app.use(errorHandler)
 
