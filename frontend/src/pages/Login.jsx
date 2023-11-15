5d260c88c6447cdc1b4daab21e48fc6228d0e00a
NeginSal <neginsaljooghi@gmail.com>
2023-11-15 11:20:01 +0330

    feat: Login Form

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,43 @@
+import { useState } from "react";
+
 const Login = () => {
-  return ( 
-    <div>
-      Login
-    </div>
-   );
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }))
+
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  const { email, password } = formData
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        name='email'
+        placeholder="Enter your email"
+        value={email}
+        onChange={onChange}
+      />
+      <input
+        type="password"
+        name='password'
+        placeholder="Enter your password"
+        value={password}
+        onChange={onChange}
+      />
+      <button>Login</button>
+
+    </form>
+  );
 }
- 
+
 export default Login;
